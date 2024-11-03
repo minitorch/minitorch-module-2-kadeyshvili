9bb573dd7c4670b6ab83619aff6247ae784381d6
Polina Kadeyshvili <pshvili2002@gmail.com>
2024-11-03 15:55:25 +0100

    module 2

--- minitorch/autodiff.py
+++ minitorch/autodiff.py
@@ -3,6 +3,8 @@ from typing import Any, Iterable, List, Tuple
 
 from typing_extensions import Protocol
 
+from collections import defaultdict
+
 # ## Task 1.1
 # Central Difference calculation
 
@@ -22,7 +24,11 @@ def central_difference(f: Any, *vals: Any, arg: int = 0, epsilon: float = 1e-6) 
     Returns:
         An approximation of $f'_i(x_0, \ldots, x_{n-1})$
     """
-    raise NotImplementedError("Need to include this file from past assignment.")
+    # TODO: Implement for Task 1.1.
+    a = [v - epsilon / 2 if i == arg else v for i, v in enumerate(vals)]
+    b = [v + epsilon / 2 if i == arg else v for i, v in enumerate(vals)]
+    grad = (f(*b) - f(*a)) / epsilon
+    return grad
 
 
 variable_count = 1
@@ -60,7 +66,22 @@ def topological_sort(variable: Variable) -> Iterable[Variable]:
     Returns:
         Non-constant Variables in topological order starting from the right.
     """
-    raise NotImplementedError("Need to include this file from past assignment.")
+    # TODO: Implement for Task 1.4.
+    result = []
+    visited = set()
+    def iter_and_push(root: Variable):
+        if root.is_constant() or root.unique_id in visited:
+            return
+
+        if not root.is_leaf():
+            for node in root.parents:
+                iter_and_push(node)
+
+        result.append(root)
+        visited.add(root.unique_id)
+
+    iter_and_push(variable)
+    return result[::-1]
 
 
 def backpropagate(variable: Variable, deriv: Any) -> None:
@@ -74,7 +95,19 @@ def backpropagate(variable: Variable, deriv: Any) -> None:
 
     No return. Should write to its results to the derivative values of each leaf through `accumulate_derivative`.
     """
-    raise NotImplementedError("Need to include this file from past assignment.")
+    # TODO: Implement for Task 1.4.
+    sorted_nodes = topological_sort(variable)
+    id_to_deriv = defaultdict(float)
+    id_to_deriv[variable.unique_id] = deriv
+
+    for node in sorted_nodes:
+        if node.is_leaf():
+            node.accumulate_derivative(id_to_deriv[node.unique_id])
+        else:
+            results = node.chain_rule(id_to_deriv[node.unique_id])
+            for input, deriv in results:
+                if not input.is_constant():
+                    id_to_deriv[input.unique_id] += deriv
 
 
 @dataclass
--- minitorch/tensor_data.py
+++ minitorch/tensor_data.py
@@ -44,7 +44,7 @@ def index_to_position(index: Index, strides: Strides) -> int:
     """
 
     # TODO: Implement for Task 2.1.
-    raise NotImplementedError("Need to implement for Task 2.1")
+    return int(sum(idx * stride for idx, stride in zip(index, strides)))
 
 
 def to_index(ordinal: int, shape: Shape, out_index: OutIndex) -> None:
@@ -61,7 +61,9 @@ def to_index(ordinal: int, shape: Shape, out_index: OutIndex) -> None:
 
     """
     # TODO: Implement for Task 2.1.
-    raise NotImplementedError("Need to implement for Task 2.1")
+    for i in range(len(shape) - 1, -1, -1):
+        out_index[i] = ordinal % shape[i]
+        ordinal //= shape[i]
 
 
 def broadcast_index(
@@ -84,7 +86,9 @@ def broadcast_index(
         None
     """
     # TODO: Implement for Task 2.2.
-    raise NotImplementedError("Need to implement for Task 2.2")
+    offset = len(big_shape) - len(shape)
+    for i in range(len(shape)):
+        out_index[i] = big_index[i + offset] if shape[i] > 1 else 0
 
 
 def shape_broadcast(shape1: UserShape, shape2: UserShape) -> UserShape:
@@ -102,7 +106,22 @@ def shape_broadcast(shape1: UserShape, shape2: UserShape) -> UserShape:
         IndexingError : if cannot broadcast
     """
     # TODO: Implement for Task 2.2.
-    raise NotImplementedError("Need to implement for Task 2.2")
+    result = []
+    len1, len2 = len(shape1), len(shape2)
+    max_len = max(len1, len2)
+    shape1 = (1,) * (max_len - len1) + shape1
+    shape2 = (1,) * (max_len - len2) + shape2
+    for s1, s2 in zip(shape1, shape2):
+        if s1 == 1:
+            result.append(s2)
+        elif s2 == 1:
+            result.append(s1)
+        elif s1 == s2:
+            result.append(s1)
+        else:
+            raise IndexingError(f"Cannot broadcast shapes {shape1} and {shape2}")
+
+    return tuple(result)
 
 
 def strides_from_shape(shape: UserShape) -> UserStrides:
@@ -223,7 +242,9 @@ class TensorData:
         ), f"Must give a position to each dimension. Shape: {self.shape} Order: {order}"
 
         # TODO: Implement for Task 2.1.
-        raise NotImplementedError("Need to implement for Task 2.1")
+        new_shape = tuple(self.shape[i] for i in order)
+        new_strides = tuple(self.strides[i] for i in order)
+        return TensorData(self._storage, new_shape, new_strides)
 
     def to_string(self) -> str:
         s = ""
